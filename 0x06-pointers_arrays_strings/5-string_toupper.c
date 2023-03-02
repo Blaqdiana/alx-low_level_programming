2c60ea98d1167ffecda7be3d310098340413bea2
Blaqdiana <dianaifunanya@gmail.com>
2023-03-02 03:55:39 -0800

    Always lool up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,7 +13,7 @@ char *string_toupper(char *string)
 
 	i = 0;
 
-	while(string[i] != '\0')
+	while (string[i] != '\0')
 	{
 		if (string[i] >= 'a' && string[i] <= 'z')
 			string[i] = (string[i] - 'a') + 'A';
