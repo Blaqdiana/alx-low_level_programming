881c077d4f0f4246f75c5e1074ae27ed550be9f2
Blaqdiana <dianaifunanya@gmail.com>
2023-04-04 09:58:26 -0700

    fixed 0-print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,10 +7,10 @@
  *
  * Return: Number of nodes.
  */
-size_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h)
 {
-	size_t i = 0
-	
+	size_t i = 0;
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
